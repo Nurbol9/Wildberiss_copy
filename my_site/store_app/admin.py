86ecc6eafe821001d8e522c242b1df3442b4a9ac
Nurbol9 <nurbol05.0909@gmail.com>
2026-09-24 10:35:53 +0600

    add

--- my_site/store_app/admin.py
+++ my_site/store_app/admin.py
@@ -4,43 +4,6 @@ from .models import (UserProfile, Category, Product,
 
 from modeltranslation.admin import TranslationAdmin,TranslationInlineModelAdmin
 
-class SubCategoryInline(admin.TabularInline,TranslationInlineModelAdmin):
-    model = SubCategory
-    extra = 1
-
-class ProductImageInline(admin.TabularInline):
-    model = ProductImage
-    extra = 1
-
-
-@admin.register(Category)
-class CategoryAdmin(TranslationAdmin):
-    inlines = [SubCategoryInline]
-    class Media:
-        js = (
-            'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
-            'http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js',
-            'modeltranslation/js/tabbed_translation_fields.js',
-        )
-        css = {
-            'screen': ('modeltranslation/css/tabbed_translation_fields.css',),
-        }
-
-
-@admin.register( Product)
-class ProductAdmin(TranslationAdmin):
-    inlines = [ProductImageInline]
-    class Media:
-        js = (
-            'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
-            'http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js',
-            'modeltranslation/js/tabbed_translation_fields.js',
-        )
-        css = {
-            'screen': ('modeltranslation/css/tabbed_translation_fields.css',),
-        }
-
-
 
 admin.site.register(UserProfile)
 admin.site.register(Review)
--- my_site/store_app/pagination.py
+++ my_site/store_app/pagination.py
@@ -1,3 +1,4 @@
+
 from rest_framework.pagination import PageNumberPagination
 
 
@@ -5,4 +6,3 @@ class ProductPagination(PageNumberPagination):
     page_size = 4
     page_size_query_param = 'page_size'
     max_page_size = 10
-
